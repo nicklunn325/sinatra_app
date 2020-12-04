3f7cf1d190645ed274c52a1769c2b7a1ec5a4af0
Nicholas Lunn <nrlunn325@gmail.com>
2020-12-04 10:17:08 -0600

    set dev db

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,7 @@ ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3',
     database: "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+configure :development do
+    set :database, 'sqlite3:db/development.db'
+  end
